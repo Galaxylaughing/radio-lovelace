0cd8bc1c728f71300e94243fcc5ec74b3b505f5e
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-26 14:05:41 -0800

    converted to functional component

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -25,72 +25,71 @@ const calculatePlayTime = (tracks) => {
   return `${hours}:${minutes}:${seconds}`;
 }
 
-class Playlist extends React.Component {
-  constructor(props) {
-    super(props);
-    this.trackCount = props.tracks.length;
-    this.playtime = calculatePlayTime(props.tracks);
-    this.state = {
-      tracks: props.tracks,
-    }
+const Playlist = (props) => {
+  console.log(props);
+  const tracks = props.tracks;
+  const trackCount = tracks.length;
+  const playtime = calculatePlayTime(tracks);
+
+  const toggleFavoriteCallback = (playlistSide, trackTitle) => {
+    this.props.toggleFavoriteCallback(playlistSide, trackTitle);
+  }
+  const moveToTopCallback = (playlistSide, trackTitle) => {
+    this.props.moveToTopCallback(playlistSide, trackTitle);
   }
 
-  toggleFavorite = (trackTitle) => {
-    // find specific track
-    const { tracks } = this.state;
-    const trackToToggle = tracks.find((track) => track.title === trackTitle);
-    // toggle favorite
-    trackToToggle.favorite = !Boolean(trackToToggle.favorite);
-    // save and re-render
-    this.setState( { tracks: tracks } );
-  }
+  // toggleFavorite = (trackTitle) => {
+  //   // find specific track
+  //   const { tracks } = this.state;
+  //   const trackToToggle = tracks.find((track) => track.title === trackTitle);
+  //   // toggle favorite
+  //   trackToToggle.favorite = !Boolean(trackToToggle.favorite);
+  //   // save and re-render
+  //   this.setState( { tracks: tracks } );
+  // }
 
-  moveToTop = ( trackTitle ) => {
-    // find specific track
-    const { tracks } = this.state;
-    const trackToMove = tracks.find( ( track ) => track.title === trackTitle );
+  // moveToTop = ( trackTitle ) => {
+  //   // find specific track
+  //   const { tracks } = this.state;
+  //   const trackToMove = tracks.find( ( track ) => track.title === trackTitle );
 
-    // if the track is not already at top of the list
-    if ( tracks[0].title !== trackTitle ) {
-      // move that track to top of the list:
-      // slice out item
-      const trackIndex = tracks.indexOf(trackToMove);
-      tracks.splice(trackIndex, 1);
-      // add back to beginning
-      tracks.unshift(trackToMove);
-    }
-    // save and re-render
-    this.setState( {tracks: tracks } );
-  }
+  //   // if the track is not already at top of the list
+  //   if ( tracks[0].title !== trackTitle ) {
+  //     // move that track to top of the list:
+  //     // slice out item
+  //     const trackIndex = tracks.indexOf(trackToMove);
+  //     tracks.splice(trackIndex, 1);
+  //     // add back to beginning
+  //     tracks.unshift(trackToMove);
+  //   }
+  //   // save and re-render
+  //   this.setState( {tracks: tracks } );
+  // }
 
-  trackElements = () => {
-    return this.state.tracks.map((track, i) => {
-      // We use "spread syntax" here to pass in all the properties of 
-      // the variable 'track' as props. Go look it up!
-      return (
-        <Track
-          key={track.id}
-          {...track}
-          toggleFavoriteCallback={ this.toggleFavorite }
-          moveToTopCallback={ this.moveToTop }
-        />
-      );
-    });
-  }
-
-  render() {
+  const trackElements = tracks.map((track, i) => {
+    // We use "spread syntax" here to pass in all the properties of 
+    // the variable 'track' as props. Go look it up!
     return (
-      <div className="playlist">
-        <h2>{this.side} Playlist</h2>
-        <p>
-          {this.trackCount} tracks - {this.playtime}
-        </p>
-        <ul className="playlist--track-list">
-          {this.trackElements()}
-        </ul>
-      </div>
+      <Track
+        key={track.id}
+        {...track}
+        toggleFavoriteCallback={ toggleFavoriteCallback }
+        moveToTopCallback={ moveToTopCallback }
+      />
     );
-  }
+  });
+
+  return (
+    <div className="playlist">
+      <h2>{props.side} Playlist</h2>
+      <p>
+        {trackCount} tracks - {playtime}
+      </p>
+      <ul className="playlist--track-list">
+        {trackElements}
+      </ul>
+    </div>
+  );
 }
 
 Playlist.propTypes = {
